d76c5a6e6ba574fe352966af3d529ca344684a47
nickboon <boonnick@hotmail.com>
2021-12-14 01:06:45 +0000

    Removing space before sut log message.

--- test/assert.js
+++ test/assert.js
@@ -1,7 +1,7 @@
 // see https://chiamakaikeanyi.dev/coloured-console-log-in-browser-devtools/
 export default class Assert {
 	sut(sut) {
-		console.log(`%c ${sut?.name || 'e2e'} tests`, 'text-decoration: underline');
+		console.log(`%c${sut?.name || 'e2e'} tests`, 'text-decoration: underline');
 	}
 
 	true(expectation, test) {
